74ebf693da4781453db013edc94832fd7cd68b2f
tylerking406 <tylerking406@gmail.com>
2024-10-06 02:46:50 +0200

    complete MergeSort Algorithm

--- Recursion/MergeSort.java
+++ Recursion/MergeSort.java
@@ -2,6 +2,17 @@ package Recursion;
 
 public class MergeSort {
 
+    public static void main(String[] args) {
+        int[] array = {4,3,1,5,8,3,9,0,7,7,3};
+        mergeSort(array);
+
+        //print elements
+        for (int i : array) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
     public static void mergeSort(int[] array){
        
         int length = array.length;
@@ -37,7 +48,46 @@ public class MergeSort {
     }
 
     private static void merge(int[] array, int[] leftArray, int[] rightArray) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'merge'");
+        
+        //cache the array sizes
+        int leftSize = array.length/2;
+        int rightSize = array.length - leftSize;
+
+        //pointer variables
+        int i=0,l=0,r=0; //index for  mergedArray leftArray rightArray
+       
+        //compare elements of both arrays and put em in merged array
+        while (l<leftSize && r<rightSize) {
+            
+            //compare 2 elements
+            if (leftArray[l]<rightArray[r]) {
+                array[i] = leftArray[l];
+                i++;
+                l++;
+            }
+            else{
+                array[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+
+        //for remaining elements
+
+        //if left was bigger
+        while (l<leftSize) {
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+
+        //if right was bigger
+        while (r<rightSize) {
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
+
+
     }
 }
